5399d4a1fc4ce13b8f9951b33a7f1c8d62dbb043
Aidan Fontano <aidanxf44@gmail.com>
2025-11-11 15:59:17 -0800

    adding final everypicture project

--- everypicture/final/script.js
+++ everypicture/final/script.js
@@ -7,6 +7,8 @@
     const mySwitch = document.querySelector('#switch');
     const docs = document.querySelector('#docs');
     const bedroom = document.querySelector('#overlay');
+    const header = document.querySelector('h1');
+    let fadeOut = false;
 
     cat.addEventListener('mouseover', function(){
         document.querySelector('#bubble-4').style.display = 'block';
@@ -16,23 +18,36 @@
     });
 
     sketchbook.addEventListener('mouseover', function(){
-        document.querySelector('#bubble-1').style.display = 'block';
+        document.querySelector('#bubble-3').style.display = 'block';
     });
     sketchbook.addEventListener('mouseout', function(){
-        document.querySelector('#bubble-1').style.display = 'none';
+        document.querySelector('#bubble-3').style.display = 'none';
     });
 
     mySwitch.addEventListener('mouseover', function(){
-        document.querySelector('#bubble-2').style.display = 'block';
+        document.querySelector('#bubble-1').style.display = 'block';
     });
     mySwitch.addEventListener('mouseout', function(){
-        document.querySelector('#bubble-2').style.display = 'none';
+        document.querySelector('#bubble-1').style.display = 'none';
     });
 
     docs.addEventListener('mouseover', function(){
-        document.querySelector('#bubble-3').style.display = 'block';
+        document.querySelector('#bubble-2').style.display = 'block';
     });
     docs.addEventListener('mouseout', function(){
-        document.querySelector('#bubble-3').style.display = 'none';
+        document.querySelector('#bubble-2').style.display = 'none';
+    });
+
+    header.addEventListener('click', function(){
+        if(fadeOut == false){
+            bedroom.style.display = 'block';
+            bedroom.classList.add('fade-in');
+            document.querySelector('h1').innerHTML= 'Not anymore...'
+            setTimeout(function(){
+                bedroom.style.opacity = '1';
+                bedroom.classList.remove('fade-in');
+                fadeOut = true;
+            },2000); 
+        }
     });
 })();
